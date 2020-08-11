086e7315b0e48fbc816ef2abbe649f89e7f01d05
Dhara <dhara.patel95@hotmail.co.uk>
2020-08-11 10:02:59 +0100

    Deposit method updates balance when money added to account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,16 @@
 require 'account.rb'
 
 describe Account do
+  describe '#initialize' do
+    it 'initializes with a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 
-  describe "#initialize" do
-    it "initializes with a balance of zero" do
-      expect(subject.balance).to equal(0)
+  describe '#deposit' do
+    it 'updates balance when money is deposited into account' do
+      subject.deposit(1000)
+      expect(subject.balance).to eq(1000)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 require 'simplecov'
@@ -8,7 +7,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
-])
+  ])
 SimpleCov.start
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
